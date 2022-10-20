74e1205776ce5ec21cabd7aad5cc0e12e71e5cbe
Dzianis <dzianis888mv@gmail.com>
2022-10-20 23:40:41 +0300

    Переделал функцию

--- JS/js.js
+++ JS/js.js
@@ -1,17 +1,37 @@
 let obj1 = {
-firstName: "Витя",
-lastName: "Киселёв",
-age: 45,
-children: null,
-dates: {
-  birthday: '26.01.1977',
-  wedding: '05.09.2000',
-  graduationFromUniversity: '07.06.2003',
-},
-}
+  imgEnot: {
+    EnotTwo: {
+      EnotThree:
+        "<br>" +
+        "<img src='https://placepic.ru/wp-content/uploads/2018/11/s1200-45.jpg' style='width:400px'>",
+    },
+  },
 
-document.write(obj2.Lessontask6);
-document.write(obj2.condition);
-document.write(obj2.imgEnot.EnotTwo.EnotThree);
-document.write(obj2.conditionTwo.conditionObjTwo);
-obj2.myFunctiun();
+  firstName: "Витя",
+  lastName: "Киселёв",
+  age: 45,
+  children: null,
+  dates: {
+    birthday: "26.01.1977",
+    wedding: "05.09.2000",
+    graduationFromUniversity: "07.06.2003",
+  },
+};
+
+getFiniteValue(obj1);
+function getFiniteValue(obj) {
+  getProp(obj);
+
+  function getProp(o) {
+    for (var prop in o) {
+      if (typeof o[prop] === "object") {
+        getProp(o[prop]);
+      } else {
+        console.log("Свойство: " + [prop]);
+        document.write(
+          "Свойство: " + [prop] + " Имеет значение:" + o[prop] + "<br>"
+        );
+      }
+    }
+  }
+}
